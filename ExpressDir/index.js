f6135c390662037ded5589ddd39e5249a603d93f
AmanTiwari2304 <amant9363@gmail.com>
2024-10-11 14:58:13 +0530

    About Nodemon and Path parameter

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -27,17 +27,34 @@ app.listen(port,()=>{
 //Method 2:-
 
 app.get("/",(req,res) => {
-    res.send("You contacted root path");
+    res.send("Use of nodemon to get this root path statement");
 });
 
-app.get("/apple",(req,res) => {
-    res.send("You contacted apple path");
-});
+// app.get("/apple",(req,res) => {
+//     res.send("You contacted apple path");
+// });
 
-app.get("/orange",(req,res) => {
-    res.send("You contacted orange path");
-});
+// app.get("/orange",(req,res) => {
+//     res.send("You contacted orange path");
+// });
 
-app.get("*",(req,res) => {
-    res.send("this path");
-});
+// app.get("*",(req,res) => {
+//     res.send("this path");
+// });
+
+//Path Parameter :-
+app.get("/:username/:id", (req,res) => {
+    let {username,id} = req.params;
+    let htmlStr = `<h1> Welcome to the page of @${username} ! </h1>`
+    res.send(htmlStr);
+    // res.send(`This account belongs to @${username} and his id is ${id}`)
+})
+
+//Query Strings:-
+app.get("/search",(req,res) => {
+    let {q} = req.query;
+    if (!q) {
+        res.send(`<h1> Nothing Search </h1>`);
+    }
+    res.send(`<h1> Search result for @${q} ! </h1>`);
+})
