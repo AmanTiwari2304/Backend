94f0d8fac305c6753c91cff31f655d653dc618fc
AmanTiwari2304 <amant9363@gmail.com>
2024-11-08 10:58:47 +0530

    MongoDB With Express

---
+++ MONGO3/init.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Chat = require("./models/chat.js");
+
+main()
+.then( () => {
+    console.log("Connection Successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+};
+
+let allChats = [
+    {
+    from : "Krishna",
+    to: "Rohit",
+    msg: "Ka ho ka haal ba",
+    created_at : new Date()
+},
+{
+    from : "Ansh",
+    to: "Rishabh",
+    msg: "Bhai aaj kis cheez ka exam hai",
+    created_at : new Date()
+},
+{
+    from : "Aryan",
+    to: "Lovkush",
+    msg: "Bhai scooty mai chalunga ",
+    created_at : new Date()
+},
+{
+    from : "Chootu",
+    to: "Rohan",
+    msg: "Es matter me kuch bhi nhi karna hai",
+    created_at : new Date()
+},
+];
+
+Chat.insertMany(allChats)
+
