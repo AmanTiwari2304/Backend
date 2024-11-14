9db5b17b48e2da1736be22f55621bb731fbe02bc
AmanTiwari2304 <amant9363@gmail.com>
2024-11-14 15:39:54 +0530

    Error Handling

--- MONGO3/index.js
+++ MONGO3/index.js
@@ -4,6 +4,7 @@ const port = 3000
 const path = require("path");
 const mongoose = require('mongoose');
 const Chat = require("./models/chat.js");
+const ExpressError = require("./ExpressError")
 
 var methodOverride = require('method-override')
 app.use(methodOverride('_method'))
@@ -20,7 +21,7 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 };
 
 let chat1 = new Chat({
@@ -43,59 +44,97 @@ app.get('/', (req, res) => res.send('Root is working'))
 
 //Index Route :-
 app.get("/chats", async (req,res) => {
-   let chats = await Chat.find();
-   console.log(chats);
-   res.render("index.ejs",{chats});
+ try {
+    let chats = await Chat.find();
+    res.render("index.ejs",{chats});
+ } catch (err) {
+    next(err)
+ }
 })
 
 //ADD NewChat Route:-
 app.get("/chats/new",(req,res) => {
+    // throw new ExpressError(404,"Page not found");
     res.render("new.ejs");
   })
   
-app.post("/chats",(req,res) => {
-    let {from,msg,to} = req.body;
-    let newChat = new Chat({
+app.post("/chats",async(req,res,next) => {
+    try {
+        let {from,msg,to} = req.body;
+        let newChat = new Chat({
         from: from,
         to: to,
         msg: msg,
         created_at: new Date(),
     });
-    newChat.save()
-    .then((res) => {
-        console.log("chat was saved")
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-    
+    await newChat.save()
      res.redirect("/chats")
+    } catch (err) {
+     next(err);   
+    }
+    
 });
 
+function asyncWrap(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((err) => next(err));
+    }
+}
+
+//New - Show Route:-
+app.get("/chats/:id/", asyncWrap (async(req,res,next) => {
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+        if(!chat) {
+            next (new ExpressError(404,"Chat not found"));
+        }
+        res.render("edit.ejs",{chat});
+      }))
+
 //Edit Route:-
-app.get("/chats/:id/edit", async(req,res) => {
-    let {id} = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs",{chat});
-  })
+app.get("/chats/:id/edit", asyncWrap(async(req,res) => {
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+        res.render("edit.ejs",{chat});
+  }))
 
 //Update Route:-
-app.put("/chats/:id", async(req,res) => {
-    let {id} = req.params;
-    let {msg : newMsg} = req.body;
-    let updatedChat = await Chat.findByIdAndUpdate(id,{msg: newMsg},{runValidators: true, new:true})
-
-    console.log(updatedChat);
-    res.redirect("/chats")
-})
+app.put("/chats/:id",asyncWrap (async(req,res) => { 
+        let {id} = req.params;
+        let {msg : newMsg} = req.body;
+        let updatedChat = await Chat.findByIdAndUpdate(id,{msg: newMsg},{runValidators: true, new:true})
+        console.log(updatedChat);
+        res.redirect("/chats") 
+}))
 
 //Delete Route:-
-app.delete("/chats/:id", async(req,res) => {
-    let {id} = req.params;
-    let deletedChat = await Chat.findByIdAndDelete(id);
-    console.log(deletedChat);
-    res.redirect("/chats")
-    
+try {
+    app.delete("/chats/:id", async(req,res) => {
+        let {id} = req.params;
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(deletedChat);
+        res.redirect("/chats")
+    })   
+} catch (error) {
+    next(err)
+}
+
+const handleValidationErr = (err) => {
+    console.log("This was a validation error. Please fallow rules");
+    console.dir(err.message);
+    return err;
+}
+app.use((err, req, res, next ) => {
+    console.log(err.name);
+    if(err.name === "ValidationError") {
+       err = handleValidationErr(err)
+    }
+    next(err);
+})
+// Error handing middleware
+  app.use((err,req,res,next) => {
+    let {status = 500, message = "Some Error Occurred"} = err;
+    res.status(status).send(message);
   })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- Middleware/app.js
+++ Middleware/app.js
@@ -1,35 +1,63 @@
-const  express = require('express')
-const app = express()
-const port = 3000
+const  express = require('express');
+const app = express();
+const port = 3000;
+const ExpressError = require("./ExpressError");
 
 //logger
-app.use((req,res,next) =>{
-    req.responseTime = new Date (Date.now()).toString();
-    console.log(req.method,req.path,req.responseTime,req.hostname)
-    next();
-})
+// app.use((req,res,next) =>{
+//     req.responseTime = new Date (Date.now()).toString();
+//     console.log(req.method,req.path,req.responseTime,req.hostname)
+//     next();
+// })
 
 const checkToken = ("/api",(req,res,next) => {
     let {token} = req.query;
     if (token === "giveaccess"){
         next();
     }
-    throw new Error("ACCESS DENIED!")
+    throw new ExpressError(401,"ACCESS DENIED!");
 })
 
 app.get("/api", checkToken, (req,res) => {
     res.send("data");
 })
 
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/random', (req, res) => {
-        res.send('this is random page!')
+     res.send('this is random page!')
 });
 
-app.use((req, res) => {
-    res.send('Page not found')
-});
+app.get("/err",(req,res) => {
+    abcd= abcd
+})
+
+app.get("/admin",(req,res) => {
+    throw new ExpressError(403,"Access to admin is forbidden")
+})
+
+// app.use((req, res) => {
+//     res.send('Page not found')
+// });
+
+// Error Handling:-
+
+app.use((err,req,res,next) => {
+    let{status = 500,message = "Some Error Occurrred"} = err;
+    res.status(status).send(message);
+    
+})
+
+
+// app.use((err,req,res,next) => {
+//     console.log("----ERROR----");
+//     res.send(err);
+//     // next(err);
+// })
+
+// app.use((err,req,res,next) => {
+//     console.log("----ERROR 2----");
+//     next();
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
